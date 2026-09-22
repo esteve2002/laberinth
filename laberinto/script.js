3a2ebcdf232a39933609da5ae2757b5ed94b55ef
esteve2002 <106371567+esteve2002@users.noreply.github.com>
2026-09-22 17:09:43 +0000

    Sep 22, 2026, 5:09 PM

--- laberinto/script.js
+++ laberinto/script.js
@@ -87,7 +87,7 @@ function pintarLaberinto(ctx, laberinto, personaje) {
       const y = f * TAMANO_CELDA;
 
       if (laberinto[f][c] === 1) {
-        ctx.fillStyle = "#2b2b2b48"; // pared
+        ctx.fillStyle = "hsl(0, 0%, 17%)"; // pared
       } else {
         ctx.fillStyle = "#f4f4f4"; // camino
       }
