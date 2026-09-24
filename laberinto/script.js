7be885403162a3baf4b226a6aa26786ec1ea9e4b
esteve2002 <106371567+esteve2002@users.noreply.github.com>
2026-09-24 14:33:32 +0000

    PM0172:33 PM267: {24PM2:33 PMe}

--- laberinto/script.js
+++ laberinto/script.js
@@ -177,7 +177,7 @@ function animar(ahora) {
 // FUNCION PARA MOVER EL PERSONAJE
 
 function moverPersonajeDelLaberinto(laberinto, personajeX, personajeY){
-  onup
+  
 }
 
 requestAnimationFrame(animar); // arranca el bucle de dibujado suave
