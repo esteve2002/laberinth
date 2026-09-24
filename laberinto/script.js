a4aba4a04b25519e820ff149dbfaeed83d2d4567
esteve2002 <106371567+esteve2002@users.noreply.github.com>
2026-09-24 17:52:52 +0000

    PM6595:52 PM267: {24PM5:52 PMe}

--- laberinto/script.js
+++ laberinto/script.js
@@ -59,7 +59,7 @@ function colocarPersonajeAleatorio(laberinto) {
 function colocarEntradaYSalidaLaberinto(personajeX, personajeY){
   for (let f = 0; f < laberinto.length; f++) {
     for (let c = 0; c < laberinto[0].length; c++) {
-      if(laberinto[f][c] != laberinto[personajeX][pero])
+      if(laberinto[f][c] != laberinto[personajeX][pers])
 
       ctx.fillStyle = laberinto[f][c] != laberinto[personajeX][personajeY]
         ? "hsla(135, 100%, 35%, 0.49)" // pared
